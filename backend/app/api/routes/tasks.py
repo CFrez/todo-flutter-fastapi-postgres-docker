52df8dfedf18a7fbf6c139310c6f8ca533e0c655
Christina Frezynski <christina@frez.me>
2024-10-14 18:03:39 -0500

    update to handle subtasks

--- backend/app/api/routes/tasks.py
+++ backend/app/api/routes/tasks.py
@@ -50,9 +50,10 @@ async def update_task(
 @router.delete("/{task_id}/", response_model=TaskInDB)
 async def delete_task(
     task_id: uuid.UUID,
+    delete_sub_trasks: bool,
     task_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> TaskInDB:
-    result = await task_repo.delete(id=task_id)
+    result = await task_repo.delete_and_update_sub_tasks(id=task_id, delete_sub_tasks=delete_sub_trasks)
     return TaskInDB.model_validate(result)
 
 
